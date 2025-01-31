401fc08a84f42d606ce33ff1ad2a965dc1ca8a0f
warek <alexandrudobrojan@gmail.com>
2025-01-31 12:51:14 +0200

    Updates

--- assets/not-found-eriLIQRU.js
+++
@@ -1,1 +0,0 @@
-import{w as o}from"./with-props-Nl8GaAsT.js";import{n as t}from"./chunk-SYFQ2XB5-ePwPyWlc.js";const n=()=>t.jsx("div",{children:"404 Not Found"}),d=o(n);export{d as default};
--- assets/with-props-Nl8GaAsT.js
+++
@@ -1,1 +0,0 @@
-import{y as t,z as e,A as o,B as p,a as s,C as u}from"./chunk-SYFQ2XB5-ePwPyWlc.js";function i(a){return function(){const r={params:t(),loaderData:e(),actionData:o(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:t(),loaderData:e(),actionData:o(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
